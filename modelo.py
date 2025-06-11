655ff8c30f1f1f27b0ad6cb58224a8a6986911d0
Tomi <2102092@ucc.edu.ar>
2025-06-11 16:17:11 -0300

    Detalles

--- modelo.py
+++ modelo.py
@@ -1,6 +1,6 @@
 # Importa la clase YOLO de la libería ultralytics, para que puedas crear y entrenar modelos con facilidad.
 from ultralytics import YOLO
-# import yaml
+import yaml
 
 if __name__ == '__main__':
     # Se crea un nuevo moselo desde cero usando la configuración de yolov8n.yaml
@@ -10,12 +10,12 @@ if __name__ == '__main__':
     model = YOLO("yolov8n.yaml")
     model.to('cuda:0')
 
-    # with open("hyp.yaml", "r") as f:
-    #   hyp = yaml.safe_load(f)
+    with open("hyp.yaml", "r") as f:
+      hyp = yaml.safe_load(f)
       
     # Se entrena el modelo con tus propios datos
     # data="./config.yaml": usa el archivo que contiene rutas de entrenamiento, validación, test, cantidad de clases y sus nombres
     # epochs=100 y patience=5: establece un maximo de 100 epocas pero en caso de que no mejore durante 5 epocas seguidas se frena el entrnamiento.
 
-    # results = model.train(data="./config.yaml", epochs=100, amp=False, workers=0, patience=5, augment=True, **hyp)
-    results = model.train(data="./config.yaml", epochs=100, amp=False, workers=0, patience=5)
+    # results = model.train(data="./config.yaml", epochs=100, amp=False, workers=0, patience=5)
+    results = model.train(data="./config.yaml", epochs=100, amp=False, workers=0, patience=5, augment=True, **hyp)
